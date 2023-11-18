41ef82ebc55d544c19b71b352203096ebc87f233
Moses <36628352+genuinemoses@users.noreply.github.com>
2023-11-18 14:05:31 +0000

    Create show-requests-with-response.java

---
+++ request-filters/show-requests-with-responses.java
@@ -0,0 +1,11 @@
+/*
+  Author: Sajeeb Lohani (https://github.com/prodigysml/)
+  Source: GitHub
+  Init Pub. Date: Nov 02, 2023
+*/
+
+if (!requestResponse.hasResponse()){
+    return false;
+}
+
+return true;
