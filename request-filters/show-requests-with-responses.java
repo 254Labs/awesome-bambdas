3afe717f6d725983b861fc566a767588cc399bd1
Moses <36628352+genuinemoses@users.noreply.github.com>
2023-11-21 10:20:52 +0000

    Add use case for this filter

--- request-filters/show-requests-with-responses.java
+++ request-filters/show-requests-with-responses.java
@@ -2,6 +2,8 @@
   Author: Sajeeb Lohani (https://github.com/prodigysml/)
   Source: GitHub
   Init Pub. Date: Nov 02, 2023
+  Use Case:
+    Filter for requests with non-empty responses.
 */
 
 if (!requestResponse.hasResponse()){
