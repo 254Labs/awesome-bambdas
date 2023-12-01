48b8dd9f04c57acfafa9ab043ff96f6050f53fbd
Moses <36628352+genuinemoses@users.noreply.github.com>
2023-12-01 13:32:53 +0530

    Create color-requests-w-error-responses.java

---
+++ utilities/color-requests-w-error-responses.java
@@ -0,0 +1,17 @@
+/*
+  Author: rtfmkiesel (https://github.com/rtfmkiesel)
+  Source: GitHub
+  Init Pub. Date: Nov 30, 2023
+  Use Case: 
+    Color requests based on error responses to differentiate between client and server errors
+*/
+
+// Color all client errors - Blue
+if (response.isStatusCodeClass(StatusCodeClass.CLASS_4XX_CLIENT_ERRORS)) {
+	requestResponse.annotations().setHighlightColor(HighlightColor.BLUE);
+}
+
+// Color all server errors - Red
+if (response.isStatusCodeClass(StatusCodeClass.CLASS_5XX_SERVER_ERRORS)) {
+	requestResponse.annotations().setHighlightColor(HighlightColor.RED);
+}
